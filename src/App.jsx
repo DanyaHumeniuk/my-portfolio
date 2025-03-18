4c396bbb22cec21aae576740186a58cb95403c39
DanyaHumeniuk <dgumenuk076@gmail.com>
2025-03-18 14:48:28 -0700

    Completed Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 
 import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import About from "./components/About"
 
 const App = () => {
   return (
@@ -10,7 +12,11 @@ const App = () => {
       
       <div className="container mx-auto px-8">
         <Navbar />
+        <Hero />
+        {/* <About /> */}
       </div>
+
+     
       
     </div>
   )
