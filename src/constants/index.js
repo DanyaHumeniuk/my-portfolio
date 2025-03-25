10cc979737a0fb7b44cfc87e29047deb452ff218
DanyaHumeniuk <dgumenuk076@gmail.com>
2025-03-24 20:55:32 -0700

    Added Projects section

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,4 @@
-import project1 from "../assets/projects/project-1.jpg";
-import project2 from "../assets/projects/project-2.jpg";
-import project3 from "../assets/projects/project-3.jpg";
-import project4 from "../assets/projects/project-4.jpg";
+import project1 from "../assets/projects/project-1.png";
 
 export const HERO_CONTENT = `I am passionate about front-end development and enjoy combining creativity with technology to build visually appealing, accessible, and highly functional websites. With a background in psychology, I have a deep understanding of user behavior, which allows me to create intuitive and engaging digital experiences. My focus is on front-end development, problem-solving, and designing interfaces that are both aesthetically pleasing and easy to navigate, ensuring seamless interactions for users.`;
 
@@ -19,33 +16,12 @@ export const EXPERIENCES = [
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
+    title: "Interactive Campus Map Web App",
     image: project1,
     description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
-  },
-  {
-    title: "Task Management App",
-    image: project2,
-    description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
-  },
-  {
-    title: "Portfolio Website",
-    image: project3,
-    description:
-      "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
-  },
-  {
-    title: "Blogging Platform",
-    image: project4,
-    description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
-  },
+      "Contributed to the frontend of a web application that displays an interactive map of campus cafeterias. Designed and implemented a responsive Contact Us page using React and Tailwind CSS, ensuring clean layout, accessibility, and mobile compatibility.",
+    technologies: ["HTML", "Tailwind CSS", "React"],
+  }
 ];
 
 export const CONTACT = {
