d1c6de5ba9f2ea2f52fff84d165deec49d93c3be
DanyaHumeniuk <dgumenuk076@gmail.com>
2025-03-25 18:27:34 -0700

    Completed web portfolio project

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,15 @@
 import { HERO_CONTENT } from "../constants"
 import profilePic from "../assets/projects/profilePhoto.png"
+import { motion } from "framer-motion"
+
+const container = (delay) => ({
+    hidden: {x: -100, opacity: 0},
+    visible: {
+        x: 0 ,
+        opacity: 1,
+        transition: {duration: 0.5, delay: delay}
+    }
+})
 
 const Hero = () => {
   return (
@@ -7,21 +17,21 @@ const Hero = () => {
         <div className="flex flex-wrap">
             <div className="w-full lg:w-1/2">
                 <div className="flex flex-col items-center lg:items-start">
-                    <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl ">
+                    <motion.h1 variants={container(0)} initial="hidden" animate="visible" className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl ">
                         Danylo Humeniuk
-                    </h1>
-                    <span className="bg-gradient-to-r from-orange-400 via-neutral-400 to-red-500 bg-clip-text text-4xl tracking-tight text-transparent">
+                    </motion.h1>
+                    <motion.span variants={container(0.5)} initial="hidden" animate="visible" className="bg-gradient-to-r from-orange-400 via-neutral-400 to-red-500 bg-clip-text text-3xl tracking-tight text-transparent">
                         Computer Science & Psychology Major at UVic
-                    </span>
-                    <p className="my-2 max-w-xl py-6 font-light tracking-tighter">
+                    </motion.span>
+                    <motion.p variants={container(1)} initial="hidden" animate="visible" className="my-2 max-w-xl py-6 font-light tracking-tighter">
                         { HERO_CONTENT }
-                    </p>
+                    </motion.p>
                 </div>
                 
             </div>
             <div className="w-full lg:w-1/2 lg:p-8">
                 <div className="flex justify-center">
-                    <img className="rounded-2xl shadow-[0px_15px_30px_theme(colors.gray.950)]" src={profilePic} alt="Danylo Humeniuk" />
+                    <motion.img initial={{x: 100, opacity: 0}} animate={{x: 0, opacity: 1}} transition={{duration: 1, delay: 1.2}} className="rounded-2xl shadow-[0px_15px_30px_theme(colors.gray.950)]" src={profilePic} alt="Danylo Humeniuk" />
                 </div>
             </div>
         </div>
