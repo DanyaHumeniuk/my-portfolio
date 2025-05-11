b3604ea105166079cddcac9f14c03ea9c45dab06
DanyaHumeniuk <dgumenuk076@gmail.com>
2025-05-10 22:49:08 -0700

    Added a resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,31 @@
 import logo from "../assets/projects/logo1.png"
-import { FaLinkedin, FaGithub, FaEnvelope, FaFileAlt } from "react-icons/fa";
+import { FaLinkedin, FaGithub, FaEnvelope, FaFileAlt, FaArrowLeft } from "react-icons/fa";
 
 
 const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between">
-        <div className="flex flex-shrink-0 items-center">
-            <img src={logo} alt="logo" className="h-20 w-20 scale-125 mx-2"/>
+      <div className="flex flex-shrink-0 items-center">
+        <img src={logo} alt="logo" className="h-20 w-20 scale-125 mx-2"/>
+      </div>
+      <div className="flex justify-center items-center gap-4 text-2xl m-8 text-neutral-400">
+        <a href="https://www.linkedin.com/in/danylo-humeniuk-aba4a8257/">
+          <FaLinkedin className="hover:text-orange-400 hover:drop-shadow-[0_0_10px_#fb923c]"/>
+        </a>
+        <a href="https://github.com/DanyaHumeniuk">
+          <FaGithub className="hover:text-orange-400 hover:drop-shadow-[0_0_10px_#fb923c]"/>
+        </a>
+        <a href="mailto:dgumenuk076@gmail.com">
+          <FaEnvelope className="hover:text-orange-400 hover:drop-shadow-[0_0_10px_#fb923c]"/>
+        </a>
+        <div className="flex flex-row justify-center items-center gap-0">
+          <a href="/Danylo_Humeniuk_Resume.pdf" target="_blank" rel="noopener noreferrer" className="">
+            <FaFileAlt className="hover:text-orange-400 hover:drop-shadow-[0_0_10px_#fb923c]"/>
+          </a>
+          <FaArrowLeft className="scale-50"/>
+          <span className="text-base">Resume</span>
         </div>
-        <div className="flex justify-center items-center gap-4 text-2xl m-8 text-neutral-400">
-            <a href="https://www.linkedin.com/in/danylo-humeniuk-aba4a8257/">
-              <FaLinkedin className="hover:text-orange-400 hover:drop-shadow-[0_0_10px_#fb923c]"/>
-            </a>
-            <a href="https://github.com/DanyaHumeniuk">
-              <FaGithub className="hover:text-orange-400 hover:drop-shadow-[0_0_10px_#fb923c]"/>
-            </a>
-            <a href="mailto:dgumenuk076@gmail.com">
-              <FaEnvelope className="hover:text-orange-400 hover:drop-shadow-[0_0_10px_#fb923c]"/>
-            </a>
-            {/* <a href="">
-              
-              <FaFileAlt className="hover:text-orange-400 hover:drop-shadow-[0_0_10px_#fb923c]"/>
-            </a> */}
-            
-            
-        </div>
+      </div>
     </nav>
   )
 }
