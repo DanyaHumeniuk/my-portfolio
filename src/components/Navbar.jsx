ed3f24325236bfeec3604db6f7db6eb62d9df800
DanyaHumeniuk <dgumenuk076@gmail.com>
2025-05-15 16:41:18 -0700

    Updated Navbar icons to display text below when hovered over

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import logo from "../assets/projects/logo1.png"
-import { FaLinkedin, FaGithub, FaEnvelope, FaFileAlt, FaArrowLeft } from "react-icons/fa";
+import { FaLinkedin, FaGithub, FaEnvelope, FaFileAlt } from "react-icons/fa";
 
 
 const Navbar = () => {
@@ -9,22 +9,24 @@ const Navbar = () => {
         <img src={logo} alt="logo" className="h-20 w-20 scale-125 mx-2"/>
       </div>
       <div className="flex justify-center items-center gap-4 text-2xl m-8 text-neutral-400">
-        <a href="https://www.linkedin.com/in/danylo-humeniuk-aba4a8257/">
-          <FaLinkedin className="hover:text-orange-400 hover:drop-shadow-[0_0_10px_#fb923c]"/>
+        <a href="https://www.linkedin.com/in/danylo-humeniuk-aba4a8257/" className="nav-item relative flex flex-col items-center group hover:drop-shadow-[0_0_10px_#fb923c]">
+          <FaLinkedin className="nav-icon transition-transform duration-300 group-hover:text-orange-400 group-hover:-translate-y-1"/>
+          <span className="nav-text absolute mt-8 text-xs opacity-0 translate-y-2 group-hover:opacity-100 group-hover:translate-y-0 transition-all duration-300">LinkedIn</span>
         </a>
-        <a href="https://github.com/DanyaHumeniuk">
-          <FaGithub className="hover:text-orange-400 hover:drop-shadow-[0_0_10px_#fb923c]"/>
+        <a href="https://github.com/DanyaHumeniuk" className="nav-item relative flex flex-col items-center group hover:drop-shadow-[0_0_10px_#fb923c]">
+          <FaGithub className="nav-icon transition-transform duration-300 group-hover:text-orange-400 group-hover:-translate-y-1"/>
+          <span className="nav-text absolute mt-8 text-xs opacity-0 translate-y-2 group-hover:opacity-100 group-hover:translate-y-0 transition-all duration-300">Github</span>
         </a>
-        <a href="mailto:dgumenuk076@gmail.com">
-          <FaEnvelope className="hover:text-orange-400 hover:drop-shadow-[0_0_10px_#fb923c]"/>
+        <a href="mailto:dgumenuk076@gmail.com" className="nav-item relative flex flex-col items-center group hover:drop-shadow-[0_0_10px_#fb923c]">
+          <FaEnvelope className="nav-icon transition-transform duration-300 group-hover:text-orange-400 group-hover:-translate-y-1"/>
+          <span className="nav-text absolute mt-8 text-xs opacity-0 translate-y-2 group-hover:opacity-100 group-hover:translate-y-0 transition-all duration-300">Contact</span>
         </a>
-        <div className="flex flex-row justify-center items-center gap-0">
-          <a href="/Danylo_Humeniuk_Resume.pdf" target="_blank" rel="noopener noreferrer" className="">
-            <FaFileAlt className="hover:text-orange-400 hover:drop-shadow-[0_0_10px_#fb923c]"/>
-          </a>
-          <FaArrowLeft className="scale-50"/>
-          <span className="text-base">Resume</span>
-        </div>
+       
+        <a href="/Danylo_Humeniuk_Resume.pdf" target="_blank" rel="noopener noreferrer" className="nav-item relative flex flex-col items-center group hover:drop-shadow-[0_0_10px_#fb923c]">
+          <FaFileAlt className="nav-icon transition-transform duration-300 group-hover:text-orange-400 group-hover:-translate-y-1"/>
+          <span className="nav-text absolute mt-8 text-xs opacity-0 translate-y-2 group-hover:opacity-100 group-hover:translate-y-0 transition-all duration-300">Resume</span>
+        </a>
+        
       </div>
     </nav>
   )
